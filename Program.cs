3c06c89c80fec34d9e7241940dc581e680f8bea2
CARLOS EDUARDO DO NASCIMENTO DE PAIVA <carloseduardonascimento0459@gmail.com>
2023-08-14 20:22:56 -0300

    Repetição for

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//IMPRIMINDO OS NÚMEROS DE 1 A 10.
+for(int x=1; x<=10; x++)
+{
+    Console.WriteLine(x);
+}
+
+//IMPRIME NÚMEROS PARES DE 1 A 20.
+for(int y= 2; y<= 20; y++)
+{
+    if (y % 2 == 0)
+    {
+        Console.WriteLine(y);
+    }
+}
